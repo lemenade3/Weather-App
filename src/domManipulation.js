2335862ce8f33c9b13f5d010c50ab2f63237347b
Oliver Browning <lemenade3@hotmail.com>
2022-11-08 21:53:54 +0000

    Display results now prints data to individual divs

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,16 +1,28 @@
 async function displayResults(results) {
-    // this function should fill container fields with information from data variables
-        console.log(results.currentCity);
-        console.log(results.currentTemp);
-        console.log(results.currentWeather);
-        console.log(results.currentMax);
-        console.log(results.currentMin);
-        console.log(results.currentHumidity);
+
+    let city = document.querySelector('#city')
+    let temp = document.querySelector('#temp')
+    let weather = document.querySelector('#weather')
+    let max = document.querySelector('#max')
+    let min = document.querySelector('#min')
+    let humidity = document.querySelector('#humidity')
+
+        city.textContent = results.currentCity;
+        temp.textContent = results.currentTemp;
+        weather.textContent = results.currentWeather;
+        max.textContent = results.currentMax;
+        min.textContent = results.currentMin;
+        humidity.textContent = results.currentHumidity;
         
         for (let i = 0; i < results.forecastObjects.length; i++) {
-                console.log(results.forecastObjects[i].dateTime) //date and time
-                console.log(results.forecastObjects[i].temp) //time temp
-                console.log(results.forecastObjects[i].weather) //weather
+
+            let time = document.querySelector(`#dateTime${i}`)
+            let temp = document.querySelector(`#temp${i}`)
+            let weather = document.querySelector(`#weather${i}`)
+            
+            time.textContent = results.forecastObjects[i].dateTime //date and time
+            temp.textContent = results.forecastObjects[i].temp //time temp
+            weather.textContent = results.forecastObjects[i].weather //weather
         }
 }
 
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -8,16 +8,27 @@ function loadPage() {
 
     let currentCity = document.createElement('div')
     currentCity.setAttribute('class', 'data')
+    currentCity.setAttribute('id', 'city')
+
     let currentTemp = document.createElement('div')
     currentTemp.setAttribute('class', 'data')
+    currentTemp.setAttribute('id', 'temp')
+
     let currentWeather = document.createElement('div')
     currentWeather.setAttribute('class', 'data')
+    currentWeather.setAttribute('id', 'weather')
+
     let currentMax = document.createElement('div')
     currentMax.setAttribute('class', 'data')
+    currentMax.setAttribute('id', 'max')
+
     let currentMin = document.createElement('div')
     currentMin.setAttribute('class', 'data')
+    currentMin.setAttribute('id', 'min')
+
     let currentHumidity = document.createElement('div')
     currentHumidity.setAttribute('class', 'data')
+    currentHumidity.setAttribute('id', 'humidity')
 
     currentContainer.append(currentCity, currentTemp, currentWeather, currentMax, currentMin, currentHumidity)
 
@@ -26,10 +37,13 @@ function loadPage() {
     for (let i = 0; i < 15; i++) {
         let dateTime = document.createElement('div')
         dateTime.setAttribute('class', 'data')
+        dateTime.setAttribute('id', `dateTime${i}`)
         let temp = document.createElement('div')
         temp.setAttribute('class', 'data')
+        temp.setAttribute('id', `temp${i}`)
         let weather = document.createElement('div')
         weather.setAttribute('class', 'data')
+        weather.setAttribute('id', `weather${i}`)
         forecastContainer.append(dateTime, temp, weather)
     }
 
